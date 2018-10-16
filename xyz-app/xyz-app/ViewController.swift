ff300f3c28d35c7ae6337cbfcd174adf0a0ee88c
Nick Burcin <nicholas.burcin@outlook.com>
2018-10-16 09:55:33 -0400

    Setup First Storyboard

---
+++ xyz-app/xyz-app/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  xyz-app
+//
+//  Created by Nicholas Burcin on 10/15/18.
+//  Copyright © 2018 Burcin Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
