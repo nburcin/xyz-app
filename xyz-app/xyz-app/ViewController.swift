2946a46a23e6752e374bef5102d7de7c2afe771a
Nick Burcin <nicholas.burcin@outlook.com>
2018-10-16 11:49:28 -0400

    Added IBOutlets and implemented CoreMotion

--- xyz-app/xyz-app/ViewController.swift
+++ xyz-app/xyz-app/ViewController.swift
@@ -7,16 +7,32 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var xLabel: UILabel!
+    
+    @IBOutlet weak var yLabel: UILabel!
+    
+    @IBOutlet weak var zLabel: UILabel!
+    
+    var motioionManager: CMMotionManager!
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        motioionManager = CMMotionManager()
+        motioionManager.startAccelerometerUpdates(to: .main, withHandler: updateLabels)
+    
     }
 
+    func updateLabels(data: CMAccelerometerData?, error: Error?) {
+        guard let accelerometerData = data else { return }
+        print(accelerometerData)
+    }
+    
 
 }
 
